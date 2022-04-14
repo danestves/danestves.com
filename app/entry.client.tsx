dd81691cc91a93795c27bd5d293bd25a10a255c4
Daniel Esteves <me+github@danestves.com>
2022-04-14 19:19:30 -0400

    ci: comment cypress until react@v18 is supported

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,8 +2,4 @@
 import { RemixBrowser } from "@remix-run/react";
 import { hydrateRoot } from "react-dom/client";
 
-if (process.env.NODE_ENV === "test") {
-  require("react-dom").hydrate(<RemixBrowser />, document);
-} else {
-  hydrateRoot(document, <RemixBrowser />);
-}
+hydrateRoot(document, <RemixBrowser />);
--- app/root.tsx
+++ app/root.tsx
@@ -37,8 +37,6 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function App() {
-  console.info("🚨", "process.env:", process.env.NODE_ENV, "🚨");
-
   return (
     <html lang="en" className="h-full">
       <head>
