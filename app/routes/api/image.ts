de113346a46e1b00a7cb7212cae94cd4c306fdef
Daniel Esteves <me+github@danestves.com>
2022-04-15 14:03:07 -0400

    feat: using custom resolver depending of path

--- app/routes/api/image.ts
+++ app/routes/api/image.ts
@@ -1,14 +1,32 @@
 // Dependencies
-import { DiskCache, fsResolver, imageLoader } from "remix-image/server";
-import type { LoaderConfig } from "remix-image/server";
+import {
+  DiskCache,
+  fetchResolver,
+  fsResolver,
+  imageLoader,
+} from "remix-image/server";
+import type { LoaderConfig, Resolver } from "remix-image/server";
 import type { LoaderFunction } from "@remix-run/node";
 
 // Internals
 import { sharpTransformer } from "~/utils/image-transformer.server";
 
+export const customResolver: Resolver = async (
+  asset,
+  url,
+  options,
+  basePath
+) => {
+  if (asset.startsWith("/") && (asset.length === 1 || asset[1] !== "/")) {
+    return fsResolver(asset, url, options, basePath);
+  } else {
+    return fetchResolver(asset, url, options, basePath);
+  }
+};
+
 const config: LoaderConfig = {
   cache: new DiskCache(),
-  resolver: fsResolver,
+  resolver: customResolver,
   selfUrl: "http://localhost:3000",
   transformer: sharpTransformer,
 };
