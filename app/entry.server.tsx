e5a78274db0ff969e81e5a42fb964b7011e7fe03
Daniel Esteves <me+github@danestves.com>
2022-04-15 00:29:17 -0400

    feat: adding streaming to the app

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,7 +1,11 @@
 // Dependencies
 import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import isbot from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
+import { PassThrough } from "stream";
 import type { EntryContext } from "@remix-run/node";
+
+let ABORT_DELAY = 5000;
 
 export default function handleRequest(
   request: Request,
@@ -9,14 +13,39 @@ export default function handleRequest(
   headers: Headers,
   context: EntryContext
 ) {
-  let markup = renderToString(
-    <RemixServer context={context} url={request.url} />
-  );
+  let callbackName = isbot(request.headers.get("user-agent"))
+    ? "onAllReady"
+    : "onShellReady";
 
-  headers.set("Content-Type", "text/html");
+  return new Promise((resolve, reject) => {
+    let didError = false;
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: statusCode,
-    headers,
+    let { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={context} url={request.url} />,
+      {
+        [callbackName]() {
+          let body = new PassThrough();
+
+          headers.set("Content-Type", "text/html");
+
+          resolve(
+            // @ts-ignore - We know this is a WritableStream
+            new Response(body, {
+              status: didError ? 500 : statusCode,
+              headers,
+            })
+          );
+          pipe(body);
+        },
+        onShellError(err) {
+          reject(err);
+        },
+        onError(error) {
+          didError = true;
+          console.error(error);
+        },
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
   });
 }
