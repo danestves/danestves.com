5c4e77c5b436e4abccd50dbe60dc3007b859494d
Daniel Esteves <me+github@danestves.com>
2022-04-18 23:59:16 -0400

    fix: getting content count instead of users

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -14,7 +14,7 @@ export const loader: LoaderFunction = async ({ request }) => {
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
     await Promise.all([
-      prisma.user.count(),
+      prisma.content.count(),
       fetch(url.toString(), { method: "HEAD" }).then((r) => {
         if (!r.ok) return Promise.reject(r);
       }),
