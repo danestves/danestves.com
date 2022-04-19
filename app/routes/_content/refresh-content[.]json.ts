e736f5446e48418ddcaf2025612848e2f71e440d
Daniel Esteves <me+github@danestves.com>
2022-04-19 01:16:53 -0400

    fix: correct return type json

--- app/routes/_content/refresh-content[.]json.ts
+++ app/routes/_content/refresh-content[.]json.ts
@@ -1,5 +1,5 @@
 // Dependencies
-import { json } from "remix-utils";
+import { json } from "@remix-run/server-runtime";
 import type { LoaderFunction } from "@remix-run/server-runtime";
 
 // Internals
@@ -7,11 +7,11 @@ import { getContentState } from "~/models/content-state.server";
 
 export const loader: LoaderFunction = async () => {
   const rows = await getContentState();
-  const data = rows || {};
+  const data = rows ?? {};
 
   return json(data, {
     headers: {
-      "content-length": Buffer.byteLength(JSON.stringify(data)).toString(),
+      "Content-Length": Buffer.byteLength(JSON.stringify(data)).toString(),
     },
   });
 };
