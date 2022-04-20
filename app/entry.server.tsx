84d9c66ae9a458212787701e7d417b35ea9e0fb7
Daniel Esteves <me+github@danestves.com>
2022-04-20 00:31:05 -0400

    feat: using renderToString

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -2,12 +2,9 @@
 import { RemixServer } from "@remix-run/react";
 import { createInstance } from "i18next";
 import Backend from "i18next-fs-backend";
-import isbot from "isbot";
 import { resolve } from "node:path";
-import { renderToPipeableStream } from "react-dom/server";
-import { etag } from "remix-etag";
+import { renderToString } from "react-dom/server";
 import { I18nextProvider, initReactI18next } from "react-i18next";
-import { PassThrough } from "stream";
 import type { EntryContext } from "@remix-run/node";
 
 // Internals
@@ -16,8 +13,6 @@ import { otherRootRouteHandlers } from "./utils/other-root-routes.server";
 import { i18n } from "./utils/i18n.server";
 
 global.ENV = getEnv();
-
-const ABORT_DELAY = 5000;
 
 export default async function handleRequest(
   request: Request,
@@ -32,7 +27,6 @@ export default async function handleRequest(
   }
 
   const instance = createInstance();
-  const callbackName = isbot(request.headers.get("user-agent")) ? "onAllReady" : "onShellReady";
   const lng = await i18n.getLocale(request);
   const ns = i18n.getRouteNamespaces(context);
 
@@ -51,37 +45,16 @@ export default async function handleRequest(
       },
     });
 
-  return new Promise((resolve, reject) => {
-    let didError = false;
+  const markup = renderToString(
+    <I18nextProvider i18n={instance}>
+      <RemixServer context={context} url={request.url} />
+    </I18nextProvider>
+  );
 
-    const { pipe, abort } = renderToPipeableStream(
-      <I18nextProvider i18n={instance}>
-        <RemixServer context={context} url={request.url} />
-      </I18nextProvider>,
-      {
-        [callbackName]() {
-          let body = new PassThrough();
+  headers.set("Content-Type", "text/html");
 
-          headers.set("Content-Type", "text/html");
-
-          // @ts-ignore - We know that the stream is a PassThrough
-          let response = new Response(body, {
-            status: didError ? 500 : statusCode,
-            headers,
-          });
-
-          resolve(etag({ request, response }));
-          pipe(body);
-        },
-        onShellError(err) {
-          reject(err);
-        },
-        onError(error) {
-          didError = true;
-          console.error(error);
-        },
-      }
-    );
-    setTimeout(abort, ABORT_DELAY);
+  return new Response("<!DOCTYPE html>" + markup, {
+    status: statusCode,
+    headers: headers,
   });
 }
