f98ac7f8c75eeea703bd360b877ac660ba8093f6
Daniel Esteves <me+github@danestves.com>
2022-04-24 16:31:45 -0400

    feat: delete association with atom feed

--- app/root.tsx
+++ app/root.tsx
@@ -78,7 +78,6 @@ export const links: LinksFunction = () => {
     { href: "/favicon-32x32.png", rel: "icon", sizes: "32x32", type: "image/png" },
     { href: "/favicon-16x16.png", rel: "icon", sizes: "16x16", type: "image/png" },
     { href: "/site.webmanifest", rel: "manifest" },
-    { href: "/blog/rss.atom", rel: "alternate", type: "application/rss+xml" },
     { href: "/blog/rss.json", rel: "alternate", type: "application/json" },
     { href: "/blog/rss.xml", rel: "alternate", type: "application/rss+xml" },
     ...seoLinks,
