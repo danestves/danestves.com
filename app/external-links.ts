a715cb73dae4abc66178c2db51ad1831919158b9
Daniel Esteves <me+github@danestves.com>
2022-07-05 18:54:30 -0400

    fix: external links

--- app/external-links.ts
+++ app/external-links.ts
@@ -1,10 +1,10 @@
 const externalLinks = {
   github: "https://github.com/danestves",
-  githubSourceCode: "https://github.com/danestves/danestves.dev",
+  githubSourceCode: "https://github.com/danestves/danestves.com",
   instagram: "https://www.instagram.com/danestves/",
   linkedin: "https://www.linkedin.com/in/danestves/",
   resume: "https://read.cv/danestves",
-  self: "https://danestves.dev",
+  self: "https://danestves.com",
   twitter: "https://twitter.com/danestves",
   twitterDm: "https://twitter.com/messages/compose?recipient_id=554765148",
   twitterFollow: "https://twitter.com/intent/user?screen_name=danestves",
