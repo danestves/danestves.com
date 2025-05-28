03c6091d5c5de552051666ab2ae5b5a4ee505588
danestves <danestves@users.noreply.github.com>
2025-05-28 13:53:21 -0400

    refactor: comment out posts collection in config.ts for future use

--- src/content/config.ts
+++ src/content/config.ts
@@ -45,22 +45,22 @@ const projects = defineCollection({
   }),
 });
 
-const posts = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    tags: z.array(z.string()),
-  }),
-});
+// const posts = defineCollection({
+//   schema: z.object({
+//     title: z.string(),
+//     pubDate: z.date(),
+//     description: z.string(),
+//     author: z.string(),
+//     image: z.object({
+//       url: z.string(),
+//       alt: z.string(),
+//     }),
+//     tags: z.array(z.string()),
+//   }),
+// });
 
 export const collections = {
   store,
   projects,
-  posts,
+  //   posts,
 };
